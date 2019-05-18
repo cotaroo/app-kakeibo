2d9e21c6f335c34798aaa5326edbc921dc556780
[user_name] <cotaro.50tal@gmail.com>
2019-05-19 00:15:41 +0900

    update routes index.html simplecalendar.scss modal.scss select.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => "items#index"
   get "/items/select" => "items#select"
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
